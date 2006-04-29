862df121ca8a865d39d8f7bee933f70d588b9417
landonf <landonf@59370e5a-4b49-0410-bf7d-136aacc55d04>
2006-04-29 06:11:54 +0000

    Whoops, forgot the header file

---
+++ OpenVPN/auth-ldap/trunk/src/ConfigLexer.h
@@ -0,0 +1,66 @@
+/*
+ * configlexer.h
+ * Configuration Lexer
+ *
+ * Author: Landon Fuller <landonf@threerings.net>
+ *
+ * Copyright (c) 2006 Three Rings Design, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of any contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef CONFIGLEXER_H
+#define CONFIGLEXER_H
+
+#include <objc/Object.h>
+#include <stdbool.h>
+
+#include <ldap.h>
+
+#include "LFAuthLDAPConfig.h"
+
+@interface ConfigLexer : Object {
+	/* Input buffer */
+	char *buffer;
+	size_t bufferLength;
+
+	/* re2c lexer state */
+	char *cursor;
+	char *limit;
+	char *marker;
+	char *token;
+	bool eoi;
+}
+
+- (void) dealloc;
+
+- (ConfigLexer *) initWithFD: (int) fd;
+
+- (void) scan;
+
+@end
+
+#endif /* CONFIGLEXER_H */
